d65e045df4accd0e5177906b6db1baeda243a48c
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-04 15:29:08 +0200

    Update database.py
    Iniziato a popolare il db

--- database.py
+++ database.py
@@ -1,9 +1,23 @@
 from pymongo import MongoClient
-client = MongoClient(port=27017)
-db=client.dati_da_analizzare
+from pathlib import Path
 
-foto={
-    'modello':'',
-    'valori':[]
-}
-db.dati_da_analizzare.insert_one(foto)
+client = MongoClient(port=27017)
+db = client.dati_da_analizzare
+collection = db.files
+
+
+
+def carica_file(a):
+    data_folder = Path("D:\Instagram-20210504T103737Z-001\Instagram\BAULETTO")
+    a = a+'.txt'
+    file_to_open = data_folder / a
+    f = open(file_to_open)
+    text = f.read()
+    text_file = {"nome_file": a, "contenuto": text}
+    collection.insert(text_file)
+
+
+carica_file("2020-11-14_14-37-01_UTC.xml")
+carica_file("2020-11-14_16-46-36_UTC.xml")
+carica_file("2020-11-14_16-47-10_UTC.xml")
+carica_file()
