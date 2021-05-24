02aee7f85f617765672b9ab3be5468752d7f9b52
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-24 14:50:04 +0200

    Ultima API

--- Progetto_moda.py
+++ Progetto_moda.py
@@ -114,8 +114,12 @@ def pred_vgg():
         # model.to(device)
         model.eval()
         predictions = model(batch_t)
-        text_valore = "Le predizioni per l\'immagine sono':"+str(predictions)
-        return text_valore
+        #text_valore = "Le predizioni per l\'immagine sono':"+str(predictions)
+        result=str(predictions)
+        index_i = result.index('[')
+        index_f = result.index(']')
+        risultato_stampa= result [index_i: index_f+2]
+        return {f'Le predizioni per l\'immagine {jpg} sono': risultato_stampa}
     else:
         return "Errore: Non hai specificato un immagine. Riprova specificando un'immagine corretta."
 
