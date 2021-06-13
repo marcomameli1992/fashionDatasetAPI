74f2c12886d8bedbd408d613627817974934d5b3
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-06-13 10:19:46 +0200

    aggiunti i commenti per pdoc

--- Progetto_moda.py
+++ Progetto_moda.py
@@ -12,8 +12,26 @@ from PIL import Image
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 """ #Fashion Dataset API"""
-"""Creazione di API per analizzare un Fashion dataset e provare a predire i futuri trend nel campo della moda. Queste API lavorano con file di testo in formato .txt o con immagini.
+""" ####Creazione di API per analizzare un Fashion dataset e provare a predire i futuri trend nel campo della moda. Queste API lavorano con file di testo in formato .txt o con immagini.
 E restituiscono i risultati in formato JSON"""
+"""####**API N°1:** CONTEGGIO DI NUMERO DI IMMAGINI PER CLASSE"""
+"""######/imm_per_classe"""
+"""Questa API restituisce un JSON contenente il numero di immagini per ogni classe. Il risultato è il seguente:
+'''
+[
+  {
+    "Le_foto_con_oggetti_della_classe_0_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_1_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_2_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_3_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_4_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_5_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_6_sono'": 0,
+    "Le_foto_con_oggetti_della_classe_7_sono'": 0
+  }
+]
+'''
+"""
 # API n.1: conteggio di numero di immagini per classe
 @app.route('/imm_per_classe', methods=['GET'])
 def im_per_classe():
@@ -25,7 +43,15 @@ def im_per_classe():
         appoggio.append(text_valore)
     return jsonify(appoggio)
 
-
+"""####**API N°2:** CONTEGGIO DI IMMAGINI CON PIU' DI UN OGGETTO DENTRO"""
+"""######/molti_oggetti/all"""
+"""Questa API restituisce un JSON contenente il numero di immagini che hanno più di un oggetto al loro interno. Il risultato è il seguente:
+'''
+{
+  "Le_foto_con_piu_oggetti_presenti_sono": 0
+}
+'''
+"""
 # API n.2: conteggio di immagini con più di un oggetto dentro
 @app.route('/molti_oggetti/all', methods=['GET'])
 def piu_di_uno():
@@ -33,7 +59,18 @@ def piu_di_uno():
     text_valore = {"Le_foto_con_piu_oggetti_presenti_sono:": piuoggettipresenti}
     return text_valore
 
-
+"""####**API N°2.1:** CONTEGGIO DI IMMAGINI CON PIU' DI UN OGGETTO DELLA STESSA CLASSE DENTRO"""
+"""######/molti_oggetti"""
+""" 
+- **Parametri:** *classe* -> una **stringa** che corrisponde al numero della classe di cui si vuole eseguire il conteggio
+"""
+"""Questa API restituisce un JSON contenente il numero di immagini che hanno più di un oggetto della stessa classe al loro interno. Il risultato è il seguente:
+'''
+{
+  "Le_foto_con_piu_di_un_oggetto_della_classe_{classe}_sono": 0
+}
+'''
+"""
 # API n.2-1: conteggio di immagini con più di un oggetto della stessa classe dentro
 @app.route('/molti_oggetti', methods=['GET'])
 def piu_stessa_classe():
@@ -48,7 +85,15 @@ def piu_stessa_classe():
     else:
         return "Errore: Non hai specificato la classe. Riprova specificando la classe."
 
-
+"""####**API N°2.2:** CONTEGGIO DI IMMAGINI CON PIU' DI UN OGGETTO DI CLASSI DIVERSE DENTRO"""
+"""######/molti_oggetti/molte_classi"""
+"""Questa API restituisce un JSON contenente il numero di immagini che hanno più di un oggetto di classi diverse al loro interno. Il risultato è il seguente:
+'''
+{
+  "Le_foto_con piu_di_un_oggetto_di_classi_diverse_sono": 0
+}
+'''
+"""
 # API n.2-2: conteggio di immagini con più di un oggetto di classi dentro
 @app.route('/molti_oggetti/molte_classi', methods=['GET'])
 def piu_uno_piu_classi():
@@ -65,7 +110,20 @@ def piu_uno_piu_classi():
     text_valore = {f'Le_foto_con_piu_di_un_oggetto_di_classi_diverse_sono': str(contatore)}
     return text_valore
 
-
+"""####**API N°3:** VALUTAZIONE DEL COLORE DOMINANTE DI UNA DETERMINATA IMMAGINE"""
+"""######/dominante"""
+""" 
+- **Parametri:** *img* -> una **stringa** che contiene il Path dell'immagine della quale si vuole estrarre il colore dominante
+"""
+"""Questa API restituisce un JSON contenente i colori dominanti di ciascun'immagine, in formato esadecimale. Il risultato è il seguente:
+'''
+{
+  "I_colori_dominanti_per_l_immagine_{img}_sono": [
+    "string"
+  ]
+}
+'''
+"""
 # API n.3: per una determinata classe quale è il colore predominante
 @app.route('/dominante')
 def colore_dominante():
@@ -92,7 +150,16 @@ def colore_dominante():
     else:
         return "Errore: Non hai specificato l'immagine. Riprova specificando l'immagine'."
 
-
+"""####**API N°4:** RICONOSCIMENTO OGGETTI PERSENTI IN UN'IMMAGINE CON MODELLO YOLOv5 """
+"""######/YOLOv5"""
+""" 
+- **Parametri:** *img* -> una **stringa** che contiene il Path dell'immagine della quale si vuole estrarre il colore dominante
+"""
+"""Questa API restituisce un JSON. Il risultato è il seguente:
+'''
+"string"
+'''
+"""
 @app.route('/YOLOv5')
 def ssd():
     if 'img' in request.args:
@@ -122,6 +189,18 @@ def ssd():
     else:
         return "Errore: Non hai specificato un'immagine. Riprova specificando un'immagine corretta."
 
+"""####**API N°5:** RICONOSCIMENTO OGGETTI PERSENTI IN UN'IMMAGINE CON MODELLO VGG"""
+"""######/predvgg"""
+""" 
+- **Parametri:** *img* -> una **stringa** che contiene il Path dell'immagine della quale si vuole estrarre il colore dominante
+"""
+"""Questa API restituisce un JSON. Il risultato è il seguente:
+'''
+{
+  "Le_predizioni_per_l_immagine_{img}_sono": "string"
+}
+'''
+"""
 # API 4: Vgg16
 @app.route('/predvgg')
 def pred_vgg():
